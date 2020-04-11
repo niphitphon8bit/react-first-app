540f57e8c4273658bff8383d7f0bd43eb45344db
niphitphon8bit <niphitphon_nt@hotmail.com>
2020-04-11 17:46:42 +0700

    use render list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { useState } from 'react'
-// import Header from './Header'
 import './App.css'
 export default function App() {
-  // const h = 'หัว';
-  const iniData = 0;
-  const [data, setData] = useState(iniData);
-  
-  // use Effect to observe state
 
-  // useEffect(() => {
-  //  console.log({data});
-  // })
+  const [data, setData] = useState([]);
 
+  function addData() {
+    console.log(data,...data);
+    setData([...data, { value: Math.random() }]);
+  }
   return (
     <div>
-      <p className="bg-red">ข้อมูลปัจุบัน : {data}</p>
+      {/* <p className="bg-red">ข้อมูลปัจุบัน : {data}</p> */}
 
-      <button onClick={()=> setData(data + 1)}>add</button>
-      <button onClick={()=> setData(data - 1)}>delete</button>
-      <button onClick={()=> setData(iniData)}>clear</button>
-
-      {/* <Header className ="bg-red" title = {h}/> */}
+      <button onClick={addData}>add</button>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>{item.value}</li>
+        ))}
+      </ul>
     </div>
   )
 }
