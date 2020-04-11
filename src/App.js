81aff3b46e9cffd8d17e9f728664a65dd0585042
niphitphon8bit <niphitphon_nt@hotmail.com>
2020-04-11 17:21:47 +0700

    add delete clear data use setData
    Signed-off-by: niphitphon8bit <niphitphon_nt@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, { useState , useEffect} from 'react'
+import React, { useState } from 'react'
 // import Header from './Header'
 import './App.css'
 export default function App() {
   // const h = 'หัว';
-  const [data, setData] = useState(0);
+  const iniData = 0;
+  const [data, setData] = useState(iniData);
   
-  useEffect(() => {
-   console.log({data});
-  })
+  // use Effect to observe state
+
+  // useEffect(() => {
+  //  console.log({data});
+  // })
 
   return (
     <div>
       <p className="bg-red">ข้อมูลปัจุบัน : {data}</p>
 
-      <button onClick={()=> setData(data + 1)}>Click</button>
+      <button onClick={()=> setData(data + 1)}>add</button>
+      <button onClick={()=> setData(data - 1)}>delete</button>
+      <button onClick={()=> setData(iniData)}>clear</button>
 
       {/* <Header className ="bg-red" title = {h}/> */}
     </div>
